241c5dcf7005bfb5b0ec5abe349ed2ef4eec16c0
Anonymous <>
2024-07-27 00:46:04 -0700

    padding test

--- tests/test_sfeatures.py
+++ tests/test_sfeatures.py
@@ -2,12 +2,19 @@ from svc_helper.sfeatures.models import RVCHubertModel
 import numpy as np
 import torch
 import librosa
+import soundfile as sf
 
 def test_sfeatures():
     rvc_model = RVCHubertModel()
 
-    data, rate = librosa.load('tests/test_speech.wav',
-        sr=RVCHubertModel.expected_sample_rate)
-    feat = rvc_model.extract_features(torch.from_numpy(data))
+    #data, rate = librosa.load('tests/test_speech.wav',
+    #    sr=RVCHubertModel.expected_sample_rate)
+    #feat = rvc_model.extract_features(torch.from_numpy(data))
 
-    print(feat.shape)
+    data, rate = librosa.load('tests/ood5_male.wav',
+        sr=RVCHubertModel.expected_sample_rate)
+    print(len(data))
+    padded_data = rvc_model.pad_audio(data)
+    sf.write('tests/test_padded.wav', padded_data, samplerate=16000)
+    feat = rvc_model.extract_features(torch.from_numpy(data))
+    #print(feat.shape)
